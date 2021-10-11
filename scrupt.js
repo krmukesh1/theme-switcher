7de5c3afc29cc99c5bbe33d7b9050f184c22ff7b
email <krmukeshsingh51@gmail.com>
2021-10-11 11:09:26 +0530

    added toggle button on menu

--- scrupt.js
+++ scrupt.js
@@ -1,4 +1,7 @@
 const themeToggler = document.querySelector(".theme-toggler");
+const theme = document.querySelector(".themes-container");
+const themeOpen = document.querySelector("#theme-open");
+const themeClose = document.querySelector("#theme-close");
 
 function themeHandler() {
   themeToggler.classList.toggle("active");
@@ -10,3 +13,23 @@ function themeHandler() {
 }
 
 themeToggler.addEventListener("click", themeHandler);
+
+document.querySelectorAll(".theme-colors .color").forEach((color) => {
+  color.onclick = () => {
+    let background = color.style.background;
+    document
+      .querySelector(":root")
+      .style.setProperty("--main-color", background);
+  };
+});
+
+themeOpen.addEventListener("click", themeClickOpen);
+function themeClickOpen() {
+  theme.classList.add("active");
+  document.body.style.paddingRight = "350px";
+}
+themeClose.addEventListener("click", themeClickClose);
+function themeClickClose() {
+  theme.classList.remove("active");
+  document.body.style.paddingRight = "0px";
+}
